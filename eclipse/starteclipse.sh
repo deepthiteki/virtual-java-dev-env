1e63bda2609998da7ff9748a986a81ddd249d7ee
Deepthi <deepthi.teki09@gmail.com>
2020-07-06 15:03:42 +0530

    First-commit

---
+++ eclipse/starteclipse.sh
@@ -0,0 +1,7 @@
+xhost +
+echo "Starting Eclipse"
+
+docker run -ti -d -e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v ~/Workspace/EclipseProjects:/home/developer/workspace/ \
+eclipse eclipse/eclipse
---
+++ get_jenkins_password.sh
@@ -0,0 +1,1 @@
+docker exec tomcat cat /root/ .jenkins/secrets/initialAdminPassword 
